5a2dd68f8cbe833db9abd5d283bf1db5bf925ccb
Konrad von Wittich <127662701+thisKonrad@users.noreply.github.com>
2023-03-30 17:26:48 +0200

    Update TO_DO_main.js

--- TO_DO_main.js
+++ TO_DO_main.js
@@ -1,37 +1,45 @@
-/** TO DO MAIN JS :: to do list :: */
+/** TO DO .. main js :: */
 
 /* The Buttons */
 const btnADD  = document.querySelector('#btnADD');
 const btnREMOVE = document.querySelector('#btnREMOVE');
 const btnCLEAR = document.querySelector('#clearALL');
+
 /* The List */
 const list = document.querySelector('#list001');
 const inptContent = document.querySelector('#newTASK');
 
 
 /*TO DO LISTE main Functions */
+
 btnADD.addEventListener('click', ()=>{
 
 /* create the new list element */
+   
 let newListItem = document.createElement('li');
 newListItem.classList.add('newListItem');
 
 /* take out the value from the input field */
+   
 let textInpt = inptContent.value; 
 
 /* write the input text in to the list */
+   
 newListItem.innerText = textInpt; 
 list.prepend(newListItem);
 
    /*stroke the text with click event */
+   
     newListItem.addEventListener('click', ()=>{
     newListItem.classList.add('checked');})
-    
+   
    /*kills the text with doubleclick event */
+   
     newListItem.addEventListener('dblclick', ()=>{
     newListItem.remove(newListItem);})
 
 /* clear the input value after input */
+   
 if(inptContent.value = " "){   
 inptContent.value = " ";
 }
@@ -41,6 +49,7 @@ inptContent.value = inptContent.value;
 });
 
 /* :: again the same for the Enter key - keyCode = 13 :: */
+
 inptContent.addEventListener('keypress', (event)=>{
     const enterKey = 13;
     if(event.keyCode === enterKey){
@@ -64,12 +73,14 @@ inptContent.addEventListener('keypress', (event)=>{
 });
 
 /* remove task 'minus' button: */
+
 btnREMOVE.addEventListener('click', ()=>{
     let removeItem = list.firstChild;
     list.removeChild(removeItem);
 });
 
 /* clear all button */
+
 btnCLEAR.addEventListener('click', ()=>{
     const clearItem = list.querySelector('li');
     list.remove(clearItem) 
